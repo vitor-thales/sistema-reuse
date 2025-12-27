b1407ca944b728866b6e9a238489e96ea33c5c96
vitor-valentin <vitor.rohling.becker@gmail.com>
2025-12-27 16:58:34 -0300

    feat: add login page and authentication system
    - add /login route and HTML page
    - implement /login/api/credentials endpoint
    - add JWT token generation utility
    - add auth middleware
    - implement token-based authentication with jsonwebtoken

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-import app from './app.js'
-import { env } from './config/env.js';
+import app from "./app.js";
+import { env } from "./config/env.js";
+import bcrypt from "bcryptjs";
 
-app.listen(env.PORT, () => {
+app.listen(env.PORT, async () => {
     console.log(`[+] Servidor rodando em http://localhost:${env.PORT}`);
 });
